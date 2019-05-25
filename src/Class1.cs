2f4a674f6cfaa90e22f487336f473022ed870dc4
Christian Horsdal <c.horsdal@gmail.com>
2019-05-25 14:14:52 +0200

    initial impl of in mem recording logger for use in tests

---
+++ src/Class1.cs
@@ -0,0 +1,51 @@
+﻿namespace InMemLogger
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using Microsoft.Extensions.DependencyInjection;
+  using Microsoft.Extensions.Logging;
+
+  public static class LoggingBuilderExtensions
+  {
+    public static ILoggingBuilder AddInMemmory(this ILoggingBuilder builder)
+    {
+      var logger = new InMemoryLogger();
+      builder.Services.AddSingleton(logger);
+      return builder.AddProvider(new InMemLoggerProvider(logger));
+    }
+  }
+
+  public class InMemLoggerProvider : ILoggerProvider
+  {
+    private readonly InMemoryLogger logger;
+
+    public InMemLoggerProvider(InMemoryLogger logger) => this.logger = logger;
+
+    public ILogger CreateLogger(string categoryName) => logger;
+
+    public void Dispose() { }
+  }
+
+  public class InMemoryLogger : ILogger
+  {
+    private readonly List<(LogLevel, Exception, string)> logLines = new List<(LogLevel, Exception, string)>();
+
+    public IEnumerable<(LogLevel Level, Exception Exception, string Message)> RecordedLogs => this.logLines.AsReadOnly();
+    public IEnumerable<(LogLevel Level, Exception Exception, string Message)> RecordedTraceLogs => this.logLines.Where(l => l.Item1 == LogLevel.Trace);
+    public IEnumerable<(LogLevel Level, Exception Exception, string Message)> RecordedDebugLogs => this.logLines.Where(l => l.Item1 == LogLevel.Debug);
+    public IEnumerable<(LogLevel Level, Exception Exception, string Message)> RecordedInformationLogs => this.logLines.Where(l => l.Item1 == LogLevel.Information);
+    public IEnumerable<(LogLevel Level, Exception Exception, string Message)> RecordedWarningLogs => this.logLines.Where(l => l.Item1 == LogLevel.Warning);
+    public IEnumerable<(LogLevel Level, Exception Exception, string Message)> RecordedErrorLogs => this.logLines.Where(l => l.Item1 == LogLevel.Error);
+    public IEnumerable<(LogLevel Level, Exception Exception, string Message)> RecordedCriticalLogs => this.logLines.Where(l => l.Item1 == LogLevel.Critical);
+
+    public IDisposable BeginScope<TState>(TState state) => null;
+
+    public bool IsEnabled(LogLevel logLevel) => true;
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+    {
+      this.logLines.Add((logLevel, exception, formatter(state, exception)));
+    }
+  }
+}
---
+++ tests/UnitTest1.cs
@@ -0,0 +1,48 @@
+namespace InMemLogger.Tests
+{
+  using Microsoft.Extensions.Logging;
+  using System;
+  using System.Linq;
+  using System.Collections.Generic;
+  using Xunit;
+  using Microsoft.Extensions.DependencyInjection;
+
+  public class InMemoryLogger_should
+  {
+    private readonly ILogger<InMemoryLogger_should> ilogger;
+    private readonly InMemoryLogger inMemLogger;
+
+    public static IEnumerable<object[]> LogLevels() =>
+      ((IEnumerable<LogLevel>) Enum.GetValues(typeof(LogLevel))).Select(v => new object[] { v });
+
+    public InMemoryLogger_should()
+    {
+      var services = new ServiceCollection()
+        .AddLogging(x => x.SetMinimumLevel(LogLevel.Trace).AddInMemmory())
+        .BuildServiceProvider();
+
+      this.ilogger = services.GetService<ILogger<InMemoryLogger_should>>();
+      this.inMemLogger = services.GetService<InMemoryLogger>();
+    }
+
+    [Theory]
+    [MemberData(nameof(LogLevels))]
+    public void record_log_messages(LogLevel level)
+    {
+      const string expected = "testing";
+      this.ilogger.Log(level, expected);
+
+      Assert.Contains(this.inMemLogger.RecordedLogs, l => l.Level == level && l.Message == expected);
+    }
+
+    [Theory]
+    [MemberData(nameof(LogLevels))]
+    public void record_exceptions(LogLevel level)
+    {
+      var expected = new Exception();
+      this.ilogger.Log(level, expected, "");
+
+      Assert.Contains(this.inMemLogger.RecordedLogs, l => l.Level == level && l.Exception == expected);
+    }
+  }
+}
